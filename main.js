639730aa29bf2248889a057a2acd2d0ddac18aed
Dominik Gryszkiewicz <dominik.gryszkiewicz95@gmail.com>
2022-02-11 23:49:31 +0100

    final version

--- main.js
+++ main.js
@@ -2,10 +2,34 @@ const shareButton = document.querySelector('.share')
 const shareActiveButton = document.querySelector('.share-active-icon')
 const shareBelt = document.querySelector('.share-belt')
 
-shareButton.addEventListener('click', () => {
-    shareBelt.classList.toggle('active')
+window.addEventListener('resize', () => {
+    document.location.reload(true)
 })
 
-shareActiveButton.addEventListener('click', () => {
-    shareBelt.classList.toggle('active')
+if (window.innerWidth < 768) {
+    shareButton.addEventListener('click', () => {
+        shareBelt.classList.add('active')
+    })
+    shareActiveButton.addEventListener('click', () => {
+        shareBelt.classList.remove('active')
+    })
+}
+
+if (window.innerWidth > 768) {
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+        placement : 'top',
+		trigger : 'click',
+        html : true,
+        content : '<div class="share-box d-flex"><p class="mb-0 text-uppercase">share</p><a href="https://www.facebook.com/"><img src="images/icon-facebook.svg" alt="facebook-icon"></a><a href="https://twitter.com/"><img src="images/icon-twitter.svg" alt="twitter-icon"></a><a href="https://www.pinterest.com/"><img src="images/icon-pinterest.svg" alt="pinterest-icon"></a></div>'
+    });
+});
+
+shareButton.addEventListener('click', ()=> {
+    shareButton.src === 'http://127.0.0.1:5500/nowe/frontendmentor/7%20-%20Article%20preview%20component/images/icon-share.svg' 
+    ?
+    shareButton.src = 'http://127.0.0.1:5500/nowe/frontendmentor/7%20-%20Article%20preview%20component/images/icon-share-active.svg' 
+    :
+    shareButton.src = 'http://127.0.0.1:5500/nowe/frontendmentor/7%20-%20Article%20preview%20component/images/icon-share.svg'
 })
+};
