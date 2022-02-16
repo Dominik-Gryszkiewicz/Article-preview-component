941dede114f03904dc79fad070cbfcbe1246824f
Dominik Gryszkiewicz <dominik.gryszkiewicz95@gmail.com>
2022-02-16 13:36:08 +0100

    add class active to share button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let shareButton = document.querySelector('.share')
+const shareButton = document.querySelector('.share')
 const shareActiveButton = document.querySelector('.share-active-icon')
 const shareBelt = document.querySelector('.share-belt')
 
@@ -17,19 +17,15 @@ if (window.innerWidth < 768) {
 
 if (window.innerWidth > 768) {
 shareButton.addEventListener('click', ()=> {
-    $(document).ready(function(){
-        $('[data-toggle="popover"]').popover({
-            placement : 'top',
-            trigger : 'click',
-            html : true,
-            content : '<div class="share-box d-flex"><p class="mb-0 text-uppercase">share</p><a href="https://www.facebook.com/"><img src="images/icon-facebook.svg" alt="facebook-icon"></a><a href="https://twitter.com/"><img src="images/icon-twitter.svg" alt="twitter-icon"></a><a href="https://www.pinterest.com/"><img src="images/icon-pinterest.svg" alt="pinterest-icon"></a></div>'
-        });
-    });
+    shareButton.classList.toggle('active');
+}) 
 
-    shareButton.src === 'http://127.0.0.1:5500/nowe/frontendmentor/7%20-%20Article%20preview%20component/images/icon-share.svg' 
-    ?
-    shareButton.src = 'http://127.0.0.1:5500/nowe/frontendmentor/7%20-%20Article%20preview%20component/images/icon-share-active.svg' 
-    :
-    shareButton.src = 'http://127.0.0.1:5500/nowe/frontendmentor/7%20-%20Article%20preview%20component/images/icon-share.svg'
-})
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+        placement : 'top',
+        trigger : 'click',
+        html : true,
+        content : '<div class="share-box d-flex"><p class="mb-0 text-uppercase">share</p><a href="https://www.facebook.com/"><img src="images/icon-facebook.svg" alt="facebook-icon"></a><a href="https://twitter.com/"><img src="images/icon-twitter.svg" alt="twitter-icon"></a><a href="https://www.pinterest.com/"><img src="images/icon-pinterest.svg" alt="pinterest-icon"></a></div>'
+    });
+});
 };
